e25da226afa121eed3032abd86a231799395d311
Henry Zeng <youhengzeng8@gmail.com>
2024-11-19 12:41:07 -0500

    make the fetch included in the trycatch block. it will make the whole project more safe

--- js/json.js
+++ js/json.js
@@ -7,18 +7,22 @@ async function populate() {
   // Introducing JavaScript Object Notation (JSON): https://json.org/
   // STEP 4: Store the URL of a JSON file in a variable */
   const url = './js/i-scream.json'
-  // STEP 5: Use the new URL to create a new request object
-  const request = new Request(url)
-  // STEP 6: Make a network request with the fetch() function, which returns a Response object
-  const response = await fetch(request)
-  // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-  const jsonObj = await response.json()
-  // STEP 8: Output the iScream JSON object to the console
-  console.log(jsonObj)
-  // STEP 9a: Invoke the populateHeader function here, then build it below
-  populateHeader(jsonObj)
-  // STEP 10a: Invoke the showTopFlavors function here, then build it below
-  showTopFlavors(jsonObj)
+  try {
+    // STEP 5: Use the new URL to create a new request object
+    const request = new Request(url)
+    // STEP 6: Make a network request with the fetch() function, which returns a Response object
+    const response = await fetch(request)
+    // STEP 7: Capture the returned Response object and covert to a JSON object using json()
+    const jsonObj = await response.json()
+    // STEP 8: Output the iScream JSON object to the console
+    console.log(jsonObj)
+    // STEP 9a: Invoke the populateHeader function here, then build it below
+    populateHeader(jsonObj)
+    // STEP 10a: Invoke the showTopFlavors function here, then build it below
+    showTopFlavors(jsonObj)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
 }
 // STEP 3b: Call the populate() function
 populate()
