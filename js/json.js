f0c0d678420706aec0109a419ff172697fa13ce8
Henry Zeng <youhengzeng8@gmail.com>
2024-11-19 12:35:13 -0500

    finish the step 3a and 3b, create asy function [populate] and call it. store url.

--- js/json.js
+++ js/json.js
@@ -3,24 +3,19 @@ const header = document.querySelector('header')
 const section = document.querySelector('section')
 
 // STEP 3a: Create the asynchronous function populate()
-
-// Introducing JavaScript Object Notation (JSON): https://json.org/
-// STEP 4: Store the URL of a JSON file in a variable */
-
-// STEP 5: Use the new URL to create a new request object
-
-// STEP 6: Make a network request with the fetch() function, which returns a Response object
-
-// STEP 7: Capture the returned Response object and covert to a JSON object using json()
-
-// STEP 8: Output the iScream JSON object to the console
-
-// STEP 9a: Invoke the populateHeader function here, then build it below
-
-// STEP 10a: Invoke the showTopFlavors function here, then build it below
-
+async function populate() {
+  // Introducing JavaScript Object Notation (JSON): https://json.org/
+  // STEP 4: Store the URL of a JSON file in a variable */
+  const url = './js/i-scream.json'
+  // STEP 5: Use the new URL to create a new request object
+  // STEP 6: Make a network request with the fetch() function, which returns a Response object
+  // STEP 7: Capture the returned Response object and covert to a JSON object using json()
+  // STEP 8: Output the iScream JSON object to the console
+  // STEP 9a: Invoke the populateHeader function here, then build it below
+  // STEP 10a: Invoke the showTopFlavors function here, then build it below
+}
 // STEP 3b: Call the populate() function
-
+populate()
 /* STEP 9b: Build out the populateHeader() function */
 function populateHeader() {
   // Create the H1 element
